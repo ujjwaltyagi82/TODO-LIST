7e26fe89cb59ab41632cc28e2f50d6705772917f
Ujjwal Tyagi <Ujjwalroger@gmail.com>
2023-03-09 14:07:30 +0530

    keep

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import './index.css'
 
 function App() {
+
+  const [input, setinput] = useState("")
+  const [items, setitem] = useState([])
+
+  const item = (task) => {
+    setinput(task.target.value)
+
+  }
+
+  const additem = () => {
+    setitem((old) => {
+      return [...old , input];
+
+    })
+    setinput("")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="Main_div">
+        <div className="Center_div">
+          <br />
+          <h1>ToDo List</h1>
+          <br />
+          <input type="text" placeholder="Add a items" value = {input} onChange={item} />
+          <button onClick={additem}> + </button>
+
+          <ol>
+
+            {items.map((values) => {
+
+              return <li> {values} </li>
+
+            })}
+
+          </ol>
+        </div>
+      </div>
+    </>
+
+
   );
 }
 
